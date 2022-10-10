2f91fdcc6acae20a806b75a37890f43da8f5bbae
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-10 14:29:17 +0700

    update test.py

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-import socket as sk
-
-sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-
-result = sock.connect_ex(('ts.qq.com', 33446))
-if result == 0:
-    result_text = "Port is open"
-    print (result_text)
-    sock.close()
-else:
-    result_text = "Port is closed"
-    print (result_text)
-    sock.close()
+import portpicker
+test_port = portpicker.pick_unused_port()
