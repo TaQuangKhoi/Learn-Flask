a8e066f3d474dd9750da5980a3ce959c24744061
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-10 09:39:17 +0700

    File test.py chạy rất oke

---
+++ test.py
@@ -0,0 +1,13 @@
+import socket as sk
+
+sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+
+result = sock.connect_ex(('ts.qq.com', 33445))
+if result == 0:
+    result_text = ": Port is open"
+    print (result_text)
+    sock.close()
+else:
+    result_text = ": Port is closed"
+    print (result_text)
+    sock.close()
