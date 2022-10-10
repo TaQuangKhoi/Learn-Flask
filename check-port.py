0c49df2742ee1a74b58bd425964945b8b23b89b3
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-10 11:42:38 +0700

    cải thiện hiệu năng bằng cách hạn chế đóng sock

--- check-port.py
+++ check-port.py
@@ -1,32 +1,30 @@
 import socket as sk
 import logging as lg
 
-sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-
 lg.basicConfig(filename="check-port.log",
                 format='%(asctime)s %(message)s',
                 filemode='w')
-
 logger = lg.getLogger(__name__)
 logger.setLevel(lg.INFO)
+sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 
-print("Bắt đầu!!")
-x = 1
-while x <= 65535:
-    try:
-        result = sock.connect_ex(('ts.qq.com', x))
-        if result == 0:
-            result_text = str(x) + ": Port is open"
-            print (result_text)
-            logger.info (result_text)
-            sock.close()
-        else:
-            result_text = str(x) + ": Port is closed"
-            print (result_text)
-            logger.info (result_text)
-            sock.close()
-    except Exception as e:
-        logger.error (e)
-        sock.close()
-    x += 1
+def check(x):
+    result = sock.connect_ex(('ts.qq.com', x))
+    if result == 0:
+        result_text = str(x) + ": Port is open"
+        print (result_text)
+        logger.info (result_text)
+    else:
+        result_text = str(x) + ": Port is closed"
+        print (result_text)
+        logger.info (result_text)
 
+def main():
+    print("Bắt đầu!!")
+    x = 1
+    while x <= 65535:
+        check(x)
+        x += 1
+    sock.close()
+
+main()
