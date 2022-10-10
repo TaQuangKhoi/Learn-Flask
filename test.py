35a567acbefc1fe38e4d34c4789412881e9fc468
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-10 11:42:45 +0700

    update test.py

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import socket as sk
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 
-result = sock.connect_ex(('ts.qq.com', 33445))
+result = sock.connect_ex(('ts.qq.com', 33446))
 if result == 0:
     result_text = "Port is open"
     print (result_text)
     sock.close()
 else:
-    result_text = ": Port is closed"
+    result_text = "Port is closed"
     print (result_text)
     sock.close()
