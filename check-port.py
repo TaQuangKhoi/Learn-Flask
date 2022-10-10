3128e7d3040a40750e2048ab9e4ee2bfdf93a7e3
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-10 14:29:20 +0700

    update check-port.py

--- check-port.py
+++ check-port.py
@@ -1,14 +1,18 @@
 import socket as sk
 import logging as lg
+from threading import Thread
 
 lg.basicConfig(filename="check-port.log",
                 format='%(asctime)s %(message)s',
                 filemode='w')
 logger = lg.getLogger(__name__)
 logger.setLevel(lg.INFO)
-sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 
-def check(x):
+sock1 = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+sock2 = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+
+
+def check(x, sock=sock1):
     result = sock.connect_ex(('ts.qq.com', x))
     if result == 0:
         result_text = str(x) + ": Port is open"
@@ -19,12 +23,32 @@ def check(x):
         print (result_text)
         logger.info (result_text)
 
-def main():
+def one():
     print("Bắt đầu!!")
     x = 1
-    while x <= 65535:
-        check(x)
+    while x <= 32767:
+        check(x, sock1)
         x += 1
-    sock.close()
+    sock1.close()
+
+def two():
+    print("Bắt đầu!!")
+    x = 32768
+    while x <= 65535:
+        check(x, sock2)
+        x += 1
+    sock2.close()
+
+def main():
+    print("Bắt đầu!!")
+
+    thread1 = Thread(target=one)
+    thread1.start()
+
+    thread2 = Thread(target=two)
+    thread2.start()
+
+    
+
 
 main()
