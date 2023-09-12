4ad09f5dbe0debde0421bb3255ca6af40f915391
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-12 19:27:50 +0700

    Try HTML Escaping

--- mynewweb2/app.py
+++ mynewweb2/app.py
@@ -8,5 +8,10 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/mylove/<name>')
+def who_is_my_love(name):
+    return 'My love is ' + name
+
+
 if __name__ == '__main__':
     app.run()
