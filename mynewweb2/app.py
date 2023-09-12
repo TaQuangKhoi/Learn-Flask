52f882ef596e9a82acd662b41672792925423936
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-12 19:45:05 +0700

    Test URL Building

--- mynewweb2/app.py
+++ mynewweb2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -20,6 +20,11 @@ def who_is_my_love(name):
         return 'My love is myself'
     return f'My love is {escape(name)}'
 
+
+with app.test_request_context():
+    print(url_for('my_love'))
+    print(url_for('my_love', next='/'))
+    print(url_for('who_is_my_love', name='Hảo Văn'))
 
 if __name__ == '__main__':
     app.run()
