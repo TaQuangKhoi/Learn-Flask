e7fb9101e73c9ef72c40d6c30f44426a7a4ba549
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-13 22:34:36 +0700

    Create database succeed

--- flask-tutorial/flaskr/db.py
+++ flask-tutorial/flaskr/db.py
@@ -3,6 +3,9 @@ import sqlite3
 import click
 from flask import current_app, g
 
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
 def get_db():
     if 'db' not in g:
@@ -20,3 +23,17 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
