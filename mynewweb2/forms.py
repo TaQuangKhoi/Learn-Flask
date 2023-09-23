587e935a6f4c6eda90a22bab0b9cbaa0f1883701
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-23 09:51:28 +0700

    Done forms.py

--- mynewweb2/forms.py
+++ mynewweb2/forms.py
@@ -1,10 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired
 
 
 class SignupForm(FlaskForm):
-    name = StringField()
-    email = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
+    name = StringField('Name', validators=[DataRequired(
+        message='Please enter your name'
+    )])
+    email = StringField('Email', validators=[
+        DataRequired(
+            message='Please enter your email'
+        ),
+        Email(
+            message='Please enter a valid email'
+        )
+    ])
+    password = PasswordField('Password', validators=[
+        DataRequired(
+            message='Please enter a password'
+        ),
+        EqualTo('confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField('Confirm Password')
     submit = SubmitField('Sign Up')
