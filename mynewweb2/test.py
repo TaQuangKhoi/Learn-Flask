662dd97669a64794deab33ceffed5f82e3eeabf4
Keios on Python AnyWhere <khoi0941@gmail.com>
2023-10-14 02:39:02 +0000

    Save code from Python Anywhere

--- mynewweb2/test.py
+++ mynewweb2/test.py
@@ -1,6 +1,6 @@
-from app import db, app
-from models import User, Task
+# from app import db, app
+# from models import User, Task
 
-with app.app_context():
-    uq = db.session.query(User).filter_by(email='ticume@mailinator.com').first()
-    print(uq.tasks)
+# with app.app_context():
+#     uq = db.session.query(User).filter_by(email='ticume@mailinator.com').first()
+#     print(uq.tasks)
