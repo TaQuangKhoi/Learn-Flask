a12efa7772ca0ac106641a44f576d817e1a80773
Tạ Quang Khôi <khoi0941@gmail.com>
2023-10-23 13:19:47 +0700

    Done create database

--- mynewweb2/models.py
+++ mynewweb2/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     email = db.Column(db.String(64), index=True, nullable=False, unique=True)
     password_hash = db.Column(db.String(64), index=True, nullable=False)
 
-    #projects = db.relationship('Project', back_populates='user')
+    projects = db.relationship('Project', back_populates='user')
 
     def __repr__(self):
         return f'<User ({self.first_name}, {self.last_name}, {self.email}, {self.password_hash})>'
@@ -23,29 +23,28 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
 
-# class Status(db.Model):
-#     status_id = db.Column(db.Integer, Sequence('status_id_seq'), primary_key=True)
-#     desc = db.Column(db.String(64), nullable=False)
-#
-#     tasks = db.relationship('Task', back_populates='status')
-#
-#     def __repr__(self):
-#         return f'<Status {self.status_id} with text {self.text}>'
+class Status(db.Model):
+    status_id = db.Column(db.Integer, Sequence('status_id_seq'), primary_key=True)
+    desc = db.Column(db.String(64), nullable=False)
+
+    tasks = db.relationship('Task', back_populates='status')
+
+    def __repr__(self):
+        return f'<Status {self.status_id} with text {self.text}>'
 
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, Sequence('task_id_seq'), primary_key=True)
     description = db.Column(db.String(128), nullable=False)
-    # isCompleted = db.Column(db.Boolean, default=False)
 
-    # project_id = db.Column(db.Integer, db.ForeignKey('project.project_id'))
-    # project = db.relationship('Project', back_populates='tasks')
+    project_id = db.Column(db.Integer, db.ForeignKey('project.project_id'))
+    project = db.relationship('Project', back_populates='tasks')
 
     priority_id = db.Column(db.Integer, db.ForeignKey('priority.priority_id'))
     priority = db.relationship('Priority', back_populates='tasks')
 
-    # status_id = db.Column(db.Integer, db.ForeignKey('status.status_id'))
-    # status = db.relationship('Status', back_populates='tasks')
+    status_id = db.Column(db.Integer, db.ForeignKey('status.status_id'))
+    status = db.relationship('Status', back_populates='tasks')
 
     deadline = db.Column(db.DateTime, nullable=False)
 
@@ -73,20 +72,20 @@ class Priority(db.Model):
         return f'<Priority {self.priority_id} with text {self.text}>'
 
 
-# class Project(db.Model):
-#     project_id = db.Column(db.Integer, Sequence('project_id_seq'), primary_key=True)
-#
-#     name = db.Column(db.String(64), nullable=False)
-#     desc = db.Column(db.String(128), nullable=False)
-#     deadline = db.Column(db.DateTime, nullable=False)
-#
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-#     user = db.relationship('User', back_populates='projects')
-#
-#     status_id = db.Column(db.Integer, db.ForeignKey('status.status_id'))
-#     status = db.relationship('Status', back_populates='projects')
-#
-#     tasks = db.relationship('Task', back_populates='project')
-#
-#     def __repr__(self):
-#         return f'<Project {self.project_id} with name {self.name}>'
+class Project(db.Model):
+    project_id = db.Column(db.Integer, Sequence('project_id_seq'), primary_key=True)
+
+    name = db.Column(db.String(64), nullable=False)
+    desc = db.Column(db.String(128), nullable=False)
+    deadline = db.Column(db.DateTime, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    user = db.relationship('User', back_populates='projects')
+
+    status_id = db.Column(db.Integer, db.ForeignKey('status.status_id'))
+    status = db.relationship('Status', back_populates='projects')
+
+    tasks = db.relationship('Task', back_populates='project')
+
+    def __repr__(self):
+        return f'<Project {self.project_id} with name {self.name}>'
