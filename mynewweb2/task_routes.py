1b15dc8965e41819125bae5ae4e8290f7ce0f6ab
Tạ Quang Khôi <khoi0941@gmail.com>
2023-10-23 16:31:53 +0700

    Done update task by project

--- mynewweb2/task_routes.py
+++ mynewweb2/task_routes.py
@@ -168,6 +168,58 @@ def edit_task():
     return redirect('/')
 
 
+@app.route('/edit_task/<projectId>', methods=['GET', 'POST'])
+def edit_task_by_project(projectId):
+    form = TaskForm()
+
+    form.priority.choices = [
+        (p.priority_id, p.desc) for p in db.session.query(models.Priority).all()
+    ]
+
+    form.status.choices = [
+        (s.status_id, s.desc) for s in db.session.query(models.Status).all()
+    ]
+
+    _user_id = session.get('user_id')
+    if _user_id:
+        user = db.session.query(models.User).filter_by(user_id=_user_id).first()
+        _task_id = request.form['hiddenTaskId']
+        project = db.session.query(models.Project).filter_by(project_id=projectId).first()
+        if _task_id:
+            if form.submitUpdate.data:
+                print('Update task', form.data)
+                _description = form.description.data
+
+                _priority_id = form.priority.data
+                _priority = db.session.query(models.Priority).filter_by(priority_id=_priority_id).first()
+
+                _status_id = form.status.data
+                _status = db.session.query(models.Status).filter_by(status_id=_status_id).first()
+
+                _deadline = form.deadline.data
+
+                task = db.session.query(models.Task).filter_by(task_id=_task_id).first()
+                task.description = _description
+                task.priority = _priority
+                task.status = _status
+                task.deadline = _deadline
+
+                db.session.commit()
+                return redirect('/project_detail/' + projectId)
+            else:
+                task = db.session.query(models.Task).filter_by(task_id=_task_id).first()
+                form.process()
+                form.description.data = task.description
+                form.priority.data = task.priority.priority_id
+                form.status.data = task.status.status_id
+                form.deadline.data = task.deadline
+                return render_template('new-task.html', form=form, user=user, task=task)
+        elif form.validate_on_submit():
+            print('Form validated')
+
+    return redirect('/')
+
+
 @app.route('/doneTask', methods=['GET', 'POST'])
 def done_task():
     _user_id = session.get('user_id')
